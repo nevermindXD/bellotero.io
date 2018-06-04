dc2ee66b119943b28a588338e0fbd87561fafeb8
Juan Carlos Olivier <juanc.olivierj@gmail.com>
2018-06-04 11:25:41 -0500

    description completed

--- src/components/Description/index.js
+++ src/components/Description/index.js
@@ -4,63 +4,68 @@ import {Row,
         Col,
         Container} from 'reactstrap';
 
+        //Crear otra columna con la imagen arriba de la description pero se muestre con movil y la otra se oculte 
 
 
 const Description = () => {
     return (
         <Container fluid className='Description'>
-            <Row>
-                <Col className=""
-                        xl={{size: 5, offset: 1 }}
-                        lg={{size: 5, offset: 1 }}
-                        mg={{size: 5, offset: 1 }}
+            <Row className="IphoneRow">
+                <Col 
+                        xl={{size: 4, offset: 1 }}
+                        lg={{size: 4, offset: 1 }}
+                        mg={{size: 4, offset: 1 }}
                         xs={12}>
-                        <p>Bellotero.io is the digital solution that <br/>
+                        <p className="Belloteroio-is-the">
+                            Bellotero.io is the digital solution that <br/>
                             gives you fast, accurate, automated accounts payable and smart, <br/>
-                            business-transforming insights. </p>
+                            business-transforming insights. 
+                        </p>
                         <br/>
                         <br/>
-                        <p>
+                        <br/>
+                        <p className="Get-the-full-picture">
                             Get the full picture.<br/>
                             In half the time.
                         </p>
-                        <p>
+                        <p className="Threads-keep-all-you">
                             Threads keep all your conversations clearly <br/>
                             separated by topic so replies won’t <br/>
                             get buried in an endless stream of group chat. 
                         </p>
-                        <p>Learn more</p>
+                        <span className="Learn-more">Learn more &rarr;</span>
                 </Col>
                 <Col className=""
-                    xl={6}
-                    lg={6}
-                    mg={6}
+                    xl={7}
+                    lg={7}
+                    mg={7}
                     xs={12}>
-                    <img alt="" src={"https://res.cloudinary.com/dfm5xzcsq/image/upload/v1527956516/intro-img-1_3x.png"} width="300px"/>
+                    <img alt="" className="intro-img-1" src={"https://res.cloudinary.com/dfm5xzcsq/image/upload/v1527956516/intro-img-1_3x.png"} />
                 </Col>
-
+                </Row>
+                <Row className="MacRow">
                 <Col className=""
                         xl={{size: 6, offset: 1 }}
                         lg={{size: 6, offset: 1 }}
                         mg={{size: 6, offset: 1 }}
                         xs={12}>
-                        <img alt="" src={"https://res.cloudinary.com/dfm5xzcsq/image/upload/v1527956516/intro-img-2_3x.png"} width="300px"/>
+                        <img alt="" src={"https://res.cloudinary.com/dfm5xzcsq/image/upload/v1527956516/intro-img-2_3x.png"} className="intro-img-2"/>
                 </Col>
-                <Col className=""
+                <Col className="MacRight"
                     xl={5}
                     lg={5}
                     mg={5}
                     xs={12}>
-                        <p>
+                        <p className="Timesaving-moneymak">
                             Timesaving, <br/>
                             moneymaking.
                         </p>
-                        <p>
+                        <p className="Belloteroio-automat">
                          Bellotero.io automatically turns your <br/>
                          threaded conversations into a searchable <br/>
                          catalog of topics.
                         </p>
-                        <p>Learn more</p>
+                        <span className="Learn-more">Learn more &rarr;</span>
                 </Col>
             </Row>
         </Container>
--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,29 +1,14 @@
-<<<<<<< HEAD
-import React,{Component} from 'react';
-import {
-    Container,
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Button} from 'reactstrap';
-import '../styles/principal.scss';
-=======
 import React,{ Component } from 'react';
->>>>>>> e13d5d955196c6baddee76af5b38565e12aac07a
 
 import MyNavbar from './Navbar';
 import Main from "./Main";
 // import Companies from './Companies';
-// import Description from './Description';
+import Description from './Description';
 // import Percentage from './Percentage';
 // import Customers from './Customers';
 // import Calculator from './Calculator';
- import Ready from './Ready';
-// import Sitemap from './Sitemap'
+import Ready from './Ready';
+import Sitemap from './Sitemap'
 import Footer from './Footer'
 
 
@@ -32,69 +17,12 @@ export default class Principal extends Component{
     render(){
         return(
             <div>
-<<<<<<< HEAD
-                <Navbar expand="md" className="bg">
-                    <NavbarBrand href="/">Bellotero.io</NavbarBrand>
-                    <NavbarToggler onClick={this.toggle} />
-                    <Collapse isOpen={this.state.isOpen} navbar>
-                        <Nav className="ml-auto">
-                        <NavItem>
-                            <NavLink href="/" className="menu-item">Features</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/" className="menu-item">Solutions</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/" className="menu-item">Stories</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/" className="menu-item">Partners</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/" className="menu-item">About</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <NavLink href="/" className="menu-item">Blog</NavLink>
-                        </NavItem>
-                        <NavItem>
-                            <Button>Request a demo</Button>
-                        </NavItem>
-                        <NavItem>
-                            <Button>Log In</Button>
-                        </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-                <Container fluid className="Bitmap">
-
-
-                </Container>
-                <Container fluid className="Rectangle-4">
-
-
-                </Container>
-                <Container fluid className="Belloterioio-1280">
-
-
-                </Container>
-                
-=======
                 <MyNavbar/>
                 <Main/>
-                 
-                {/* 
                 <Description/>
-                <Companies/>
-                <Description/>
-                <Customers/>
-                <Calculator/>
-                <Sitemap /> 
-                <Footer />
-                <Percentage/>
-                <Ready/> */}
                 <Ready/>
-                <Footer />
->>>>>>> e13d5d955196c6baddee76af5b38565e12aac07a
+                <Sitemap/> 
+                <Footer/>
             </div>
         )
     }
